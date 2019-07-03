7f761dbfadbaed8a8fb84f3407f93ffe0b249a09
vaniusmax <vaniusmax@gmail.com>
2019-07-03 19:32:44 -0300

    primeiro commit do projeto

---
+++ src/main/java/br/com/workshopmongodb/WorkshopSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootMongodbApplication.class, args);
+	}
+
+}
