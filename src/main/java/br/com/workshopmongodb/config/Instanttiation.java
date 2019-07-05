819f3b310e3277b7abd863ef5733a5490053d42f
vaniusmax <vaniusmax@gmail.com>
2019-07-05 06:54:53 -0300

    implementando CommandLineRunner

---
+++ src/main/java/br/com/workshopmongodb/config/Instanttiation.java
@@ -0,0 +1,31 @@
+package br.com.workshopmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.workshopmongodb.domain.User;
+import br.com.workshopmongodb.repositories.UserRepository;
+
+@Configuration
+public class Instanttiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		userRepo.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		userRepo.saveAll(Arrays.asList(maria,alex,bob));
+		
+	}
+
+}
